727aa017a0689b2d112edab46222b3cb7ff07236
Kade Illian <kadeillianmt@gmail.com>
2022-11-07 12:38:29 -0700

    added url to images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_07_190144) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_07_193756) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_190144) do
     t.integer "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "url"
   end
 
   create_table "users", force: :cascade do |t|
